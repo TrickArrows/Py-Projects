7cecc7ff94001eac9eaaf59a65ee0112f7205f65
sarath <trickarrow1@gmail.com>
2022-12-07 02:41:36 +0530

    new projects

---
+++ 4.Sudoku Puzzle Solver.py
@@ -0,0 +1,84 @@
+
+def find_next_empty(puzzle):
+
+    #finds next empty r,c rep by -1
+    #otherwise return None, None
+    
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == -1:
+                return r,c
+
+    return None, None
+
+def is_valid(puzzle, guess, row, col):
+
+    row_vals = puzzle [row]
+    if guess in row_vals:
+        return False
+
+    #col_vals = []
+    #for i in range(9):
+    #  col_vals.append(puzzle[i][col]);
+    col_vals =[puzzle[i][col] for i in range(9)];
+    
+    if guess in col_vals:
+        return False
+
+    row_start = (row // 3) *3;
+    col_start = (col // 3) * 3;
+    for r in range(row_start, row_start + 3):
+        for c in range(col_start, col_start +3):
+            if puzzle[r][c] == guess:
+                return False
+            
+    return True
+
+    
+
+def solve_sudoku(puzzle):
+
+    #backtracking technique
+    #9X9 board, mutable
+
+    #step 1 - choose somewhere on the puzzle to guess
+    row,col = find_next_empty(puzzle)
+
+    
+    if row is None:     #we solved the puzzle because of row or col is None
+        return True
+
+    for guess in range(1,10):
+
+        if is_valid(puzzle, guess, row, col):
+            puzzle[row][col] = guess;
+            
+            if solve_sudoku(puzzle):
+                return True
+            
+        puzzle[row][col] = -1;
+        
+    return False
+
+if __name__ == '__main__':
+    example_board = [
+        [3,9,-1,-1,5,-1,-1,-1,-1],
+        [-1,-1,-1,2,-1,-1,-1,-1,5],
+        [-1,-1,-1,7,1,9,-1,8,-1],
+        [-1,5,-1,-1,6,8,-1,-1,-1],
+        [2,-1,6,-1,-1,3,-1,-1,-1],
+        [-1,-1,-1,-1,-1,-1,-1,-1,4],
+        [5,-1,-1,-1,-1,-1,-1,-1,-1],
+        [6,7,-1,-1,-1,5,-1,4,-1],
+        [1,-1,9,-1,-1,-1,2,-1,-1]]
+
+    for i in example_board:
+        print(i);
+        
+    print("\nSudoku Solver !! \n"+"Statue:",solve_sudoku(example_board));
+    
+    for i in example_board:
+        print(i);
+        
+        
+    
